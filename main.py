5a9c4e00a14c254b41e512038a297478fc2f4683
LeowWB <41581512+LeowWB@users.noreply.github.com>
2020-04-18 23:50:23 +0800

    Create method to iteratively run experiments with the same set of random parameters

--- main.py
+++ main.py
@@ -7,9 +7,16 @@ from random import *
 
 from pycryptosat import Solver
 
-def main(): 
+def main():
     s = Solver()
-    print(experiment_once(s, 3, 150, 10))
+    print(experiment_many(3, 150, 10, 50))
+
+def experiment_many(k, n, r, times):
+    sat_count = 0
+    for i in range(times):
+        if experiment_once(Solver(), k, n, r):
+            sat_count += 1
+    return sat_count
 
 def experiment_once(s, k, n, r):
     for i in range(r * n):
